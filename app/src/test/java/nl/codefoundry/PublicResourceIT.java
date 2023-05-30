768fb418e8eb851a09a591aeda66a856d3f09152
Adolfo Benedetti <adolfo.benedetti-velasquez@ictu.nl>
2023-05-30 10:35:21 +0200

    removed app

--- app/src/test/java/nl/codefoundry/PublicResourceIT.java
+++
@@ -1,8 +0,0 @@
-package nl.codefoundry;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class PublicResourceIT extends PublicResourceTest {
-    // Execute the same tests but in packaged mode.
-}
